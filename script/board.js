9d2e6d6674ce85f4c3411d7b3d6a7736adcbe653
David Hofer <chassis.poppets0x@icloud.com>
2024-07-11 17:07:26 +0200

    Board Elements and JS

--- script/board.js
+++ script/board.js
@@ -1,3 +1,181 @@
+let BASE_URL = 'https://taskstest-a5c3d-default-rtdb.europe-west1.firebasedatabase.app/';
+let tasks = [];
+let currentDraggedElement;
+
+
+async function initBoard() {
+    try {
+        await getDataFromFirebase();
+        // await postDataToFirebase('tasks', {'description': "rewfrewfe", "title": "title",  "status": "toDo", "category": "User Story", "assignedTo": {"name": "vorname", "surname": "secname"}, "date": "05/09/24", "prio": "urgent", "subtasks": {"one": "one", "two": "two"}});
+        await pushDataToArray();
+        initDragDrop();
+    } catch (error) {
+        console.error('dh Initialisation error:', error);
+    }
+}
+
+
+async function getDataFromFirebase(path = '') {
+    try {
+        let response = await fetch(BASE_URL + path + '.json');
+        let responseAsJson = await response.json();
+        return responseAsJson;
+    } catch (error) {
+        console.error('dh Error fetching data:', error);
+    }
+}
+
+
+async function pushDataToArray() {
+    try {
+        let tasksData = await getDataFromFirebase('tasks');
+        console.log('notes Firebase: ', tasksData);
+        for (const key in tasksData) {
+            const singleTask = tasksData[key];
+            let task = {
+                "id": key,
+                "assignedTo": singleTask.assignedTo,
+                "category": singleTask.category,
+                "date": singleTask.date,
+                "description": singleTask.description,
+                "prio": singleTask.prio,
+                "status": singleTask.status,
+                "subtasks": singleTask.subtasks,
+                "title": singleTask.title
+            };
+            tasks.push(task);
+        }
+    } catch (error) {
+        console.error('dh Error pushing tasks to array:', error);
+    }
+    console.log('tasks Array: ', tasks);
+}
+
+
+async function postDataToFirebase(path = '', data = {}) {
+    try {
+        let response = await fetch(BASE_URL + path + '.json', {
+            method: 'POST',
+            header: {
+                'Content-Type':' application/json',
+            },
+            body: JSON.stringify(data)
+        });
+        return await response.json();
+    } catch (error) {
+        console.error('dh Error posting data:', error);
+    }
+}
+
+
+async function updateTaskInFirebase(id, updatedTask) {
+    try {
+        let response = await fetch(`${BASE_URL}tasks/${id}.json`, {
+            method: 'PUT',
+            header: {
+                'Content-Type':' application/json',
+            },
+            body: JSON.stringify(updatedTask)
+        });
+        return await response.json();
+    } catch (error) {
+        console.error('dh Error putting data:', error);
+    }
+}
+
+
+function initDragDrop() {
+    updateToDo();
+    updateInProgress();
+    updateAwaitFeedback();
+    updateDone();
+    dragDrop();
+}
+
+
+function dragDrop() {
+    document.querySelectorAll('.todoContainer').forEach(todoContainer => {
+        todoContainer.addEventListener('dragstart', (e) => {
+            e.target.classList.add('tilted');
+            startDragging(e.target.id);
+        });
+        todoContainer.addEventListener('dragend', (e) => {
+            e.target.classList.remove('tilted');
+        });
+    });
+    document.querySelectorAll('.dropzone').forEach(zone => {
+        zone.addEventListener('dragover', allowDrop);
+        zone.addEventListener('drop', (e) => {
+            e.preventDefault();
+            moveTo(zone.id);
+        });
+    });
+}
+
+
+function updateToDo() {
+    let toDo = tasks.filter(t => t['status'] == 'toDo');
+    document.getElementById('toDo').innerHTML = '';
+    if (toDo.length > 0) {
+        toDo.forEach(element => {document.getElementById('toDo').innerHTML += generateTodoHTML(element);});
+    } else {
+        document.getElementById('toDo').innerHTML = `<div class="noTaskPlaceholder">No tasks To do</div>`;
+    }
+}
+
+
+function updateInProgress() {
+    let inProgress = tasks.filter(t => t['status'] == 'inProgress');
+    document.getElementById('inProgress').innerHTML = '';
+    if (inProgress.length > 0) {
+        inProgress.forEach(element => {document.getElementById('inProgress').innerHTML += generateTodoHTML(element);});
+    } else {
+        document.getElementById('inProgress').innerHTML = `<div class="noTaskPlaceholder">No tasks To do</div>`;
+    }
+}
+
+
+function updateAwaitFeedback() {
+    let awaitFeedback = tasks.filter(t => t['status'] == 'awaitFeedback');
+    document.getElementById('awaitFeedback').innerHTML = '';
+    if (awaitFeedback.length > 0) {
+        awaitFeedback.forEach(element => {document.getElementById('awaitFeedback').innerHTML += generateTodoHTML(element);});
+    } else {
+        document.getElementById('awaitFeedback').innerHTML = `<div class="noTaskPlaceholder">No tasks To do</div>`;
+    }
+}
+
+
+function updateDone() {
+    let done = tasks.filter(t => t['status'] == 'done');
+    document.getElementById('done').innerHTML = '';
+    if (done.length > 0) {
+        done.forEach(element => {document.getElementById('done').innerHTML += generateTodoHTML(element);});
+    } else {
+        document.getElementById('done').innerHTML = `<div class="noTaskPlaceholder">No tasks Done</div>`;
+    }
+}
+
+
+function startDragging(id) {
+    currentDraggedElement = id;
+}
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+
+async function moveTo(status) {
+    let task = tasks.find(task => task.id == currentDraggedElement);
+    if (task) {
+        task.status = status;
+        initDragDrop();
+        await updateTaskInFirebase(task.id, task);
+    }
+}
+
 function toggleDisplay() {
     const mobileHeader = document.getElementById('boardHeaderMobile');
     const mobileSearchContainer = document.getElementById('boardSearchContainerMobile');
@@ -5,18 +183,13 @@ function toggleDisplay() {
     const desktopSearchContainer = document.getElementById('boardHeaderSearchContainerDesktop');
 
     if (window.innerWidth >= 768) {
-        mobileHeader.style.display = 'none';
-        mobileSearchContainer.style.display = 'none';
-        desktopHeader.style.display = 'flex';
-        desktopSearchContainer.style.display = 'flex';
+        switchUiToDesktop(mobileHeader, mobileSearchContainer, desktopHeader, desktopSearchContainer);
     } else {
-        mobileHeader.style.display = 'flex';
-        mobileSearchContainer.style.display = 'flex';
-        desktopHeader.style.display = 'none';
-        desktopSearchContainer.style.display = 'none';
+        switchUiToMobile(mobileHeader, mobileSearchContainer, desktopHeader, desktopSearchContainer);
     }
 }
 
-// Event Listener für das Laden der Seite und die Größenänderung des Fensters
 window.addEventListener('load', toggleDisplay);
 window.addEventListener('resize', toggleDisplay);
+
+
---
+++ script/templates/boardtemplate.js
@@ -0,0 +1,88 @@
+const badgeColors = ['rgb(255, 121, 0)', 'rgb(29, 215, 193)', 'rgb(71, 47, 138)', 'rgb(255, 187, 43)', 'rgb(252, 113, 255)', 'rgb(110, 82, 255)', 'rgb(148, 38, 255)', 'rgb(255, 69, 70)'];
+
+
+function switchUiToDesktop(mobileHeader, mobileSearchContainer, desktopHeader, desktopSearchContainer) {
+    mobileHeader.style.display = 'none';
+    mobileSearchContainer.style.display = 'none';
+    desktopHeader.style.display = 'flex';
+    desktopSearchContainer.style.display = 'flex';
+}
+
+
+function switchUiToMobile(mobileHeader, mobileSearchContainer, desktopHeader, desktopSearchContainer) {
+    mobileHeader.style.display = 'flex';
+    mobileSearchContainer.style.display = 'flex';
+    desktopHeader.style.display = 'none';
+    desktopSearchContainer.style.display = 'none';
+}
+
+
+function generateTodoHTML(element) {
+  let categoryHTML = generateCategoryHTML(element.category);
+  let subtasksHTML = generateSubtasksHTML(element.subtasks);
+  let assignedToHTML = generateAssignedToHTML(element.assignedTo);
+  let prioHTML = generatePrioHTML(element.prio);
+
+  return /*html*/ `
+        <div draggable="true" id="${element.id}" class="todoContainer">
+            <div class="toDoContent">
+                ${categoryHTML}
+                <div>${element.title}</div>
+                <div>${element.description}</div>
+                ${subtasksHTML}
+                <div class="toDoContentBottomContainer">
+                    <div class="assignedToBadgeContainer">${assignedToHTML}</div>
+                    ${prioHTML}
+                </div>
+            </div>
+        </div>`;
+}
+
+
+function generatePrioHTML(prio) {
+    let prioHTML = '';
+    if (prio == 'urgent') {
+        prioHTML = `<img src="../assets/icons/priourgent.png">`;
+    } else if (prio == 'medium') {
+            prioHTML = `<img src="../assets/icons/priomedium.png">`;
+        } else {
+            prioHTML = `<img src="../assets/icons/priolow.png">`;
+        }
+    return prioHTML;
+}
+
+
+function generateCategoryHTML(category) {
+    let categoryHTML = '';
+    if (category == 'User Story') {
+        categoryHTML = `<div class="userStoryBadge">User Story</div>`;
+    } else {
+        categoryHTML = `<div class="technicalTaskBadge">Technical Task</div>`;
+    }
+    return categoryHTML;
+}
+
+
+function generateSubtasksHTML(subtasks) {
+  let subtasksHTML = "";
+  if (subtasks && Object.keys(subtasks).length > 1) {
+    subtasksHTML = `${Object.keys(subtasks).length} Subtasks`;
+  }
+  return subtasksHTML;
+}
+
+
+function generateAssignedToHTML(assignedTo) {
+    let assignedToHTML = '';
+    for (let i = 0; i < assignedTo.length; i++) {
+        const color = getRandomColor(badgeColors);
+        assignedToHTML += `
+            <div class="assignedToBadge" style="background-color: ${color};">${assignedTo[i].match(/(\b\S)?/g).join("").match(/(^\S|\S$)?/g).join("").toUpperCase()}</div>`;
+    }
+    return assignedToHTML;
+}
+
+
+function getRandomColor(badgeColors) {
+    return badgeColors[Math.floor(Math.random() * badgeColors.length)];
+}
