624bb426ee60e9fd8584e7499a3fc97771fd7aa7
David Hofer <chassis.poppets0x@icloud.com>
2024-07-11 22:05:55 +0200

    Header Responsive

---
+++ assets/templates/js/header.js
@@ -0,0 +1,26 @@
+function toggleHeader() {
+    const headerContainerMobile = document.getElementById('headerContainerMobile');
+    const headerContainerDesktop = document.getElementById('headerContainerDesktop');
+
+    if (window.innerWidth >= 1200) {
+        switchHeaderToDesktop(headerContainerDesktop, headerContainerMobile);
+    } else {
+        switchHeaderToMobile(headerContainerDesktop, headerContainerMobile);
+    }
+}
+
+
+function switchHeaderToDesktop(headerContainerDesktop, headerContainerMobile) {
+    headerContainerMobile.style.display = 'none';
+    headerContainerDesktop.style.display = 'flex';
+}
+
+
+function switchHeaderToMobile(headerContainerDesktop, headerContainerMobile) {
+    headerContainerMobile.style.display = 'flex';
+    headerContainerDesktop.style.display = 'none';
+}
+
+
+window.addEventListener('load', toggleHeader);
+window.addEventListener('resize', toggleHeader);
