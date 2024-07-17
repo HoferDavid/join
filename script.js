c214789268467ab27a0607fa3565e93c04354341
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2024-07-17 02:43:31 +0200

    reworked header

--- script.js
+++ script.js
@@ -34,3 +34,9 @@ function setActive() {
     }
   });
 }
+
+function toggleClass(menu, className1, className2) {
+  let edit = document.getElementById(menu);
+  edit.classList.toggle(className1);
+  edit.classList.toggle(className2);
+}
--- script/contacts.js
+++ script/contacts.js
@@ -1,5 +1,1 @@
-function toggleClass(menu, className1, className2) {
-  let edit = document.getElementById(menu);
-  edit.classList.toggle(className1);
-  edit.classList.toggle(className2);
-}
+
