51a54831cd1dcdb204d6ddc84e7102f7e0ab9f05
David Hofer <chassis.poppets0x@icloud.com>
2024-07-24 13:15:01 +0200

    Edit Task Style

--- script/board.js
+++ script/board.js
@@ -1,7 +1,6 @@
 let currentDraggedElement;
 let currentSearchInput = "";
 
-
 async function initBoard() {
   init();
   try {
@@ -15,12 +14,10 @@ async function initBoard() {
   }
 }
 
-
 function initDragDrop() {
   updateAllTaskCategories();
   dragDrop();
 }
-
 
 async function pushDataToArray() {
   try {
@@ -37,7 +34,6 @@ async function pushDataToArray() {
   }
 }
 
-
 async function createTask(key, singleTask) {
   return {
     "id": key,
@@ -52,50 +48,53 @@ async function createTask(key, singleTask) {
   };
 }
 
-
 async function updateTaskInFirebase(id, updatedTask) {
   try {
     let response = await fetch(`${BASE_URL}tasks/${id}.json`, {
-        method: "PUT",
-        header: {
-            "Content-Type": " application/json",
-        },
-        body: JSON.stringify(updatedTask),
-      });
-      return await response.json();
+      method: "PUT",
+      header: {
+        "Content-Type": " application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    });
+    return await response.json();
   } catch (error) {
-      console.error("dh Error putting data:", error);
+    console.error("dh Error putting data:", error);
   }
 }
-
 
 function toggleVisibility(id) {
   document.getElementById(id).classList.toggle("dNone");
   return document.getElementById(id);
 }
 
-
 function dragDrop() {
   document.querySelectorAll(".todoContainer").forEach((todoContainer) => {
-    todoContainer.addEventListener("dragstart", (e) => { e.target.classList.add("tilted");
-      startDragging(e.target.id); });
-    todoContainer.addEventListener("dragend", (e) => { e.target.classList.remove("tilted"); });
+    todoContainer.addEventListener("dragstart", (e) => {
+      e.target.classList.add("tilted");
+      startDragging(e.target.id);
+    });
+    todoContainer.addEventListener("dragend", (e) => {
+      e.target.classList.remove("tilted");
+    });
   });
-  document.querySelectorAll(".dropzone").forEach((zone) => { zone.addEventListener("dragover", allowDrop);
-    zone.addEventListener("drop", (e) => { e.preventDefault();
+  document.querySelectorAll(".dropzone").forEach((zone) => {
+    zone.addEventListener("dragover", allowDrop);
+    zone.addEventListener("drop", (e) => {
+      e.preventDefault();
       moveTo(zone.id);
     });
   });
 }
 
-
 function updateTaskCategories(status, categoryId, noTaskMessage) {
   let taskForSection = tasks.filter((task) => task.status === status);
   let categoryElement = document.getElementById(categoryId);
-  categoryElement.innerHTML = '';
+  categoryElement.innerHTML = "";
 
   if (taskForSection.length > 0) {
-    taskForSection.forEach((element) => { categoryElement.innerHTML += generateTodoHTML(element); 
+    taskForSection.forEach((element) => {
+      categoryElement.innerHTML += generateTodoHTML(element);
       if (element.subtasks && element.subtasks.length > 0) {
         updateSubtasksProgressBar(element.subtasks, element.id);
       }
@@ -105,24 +104,24 @@ function updateTaskCategories(status, categoryId, noTaskMessage) {
   }
 }
 
-
 function updateAllTaskCategories() {
   updateTaskCategories("toDo", "toDo", "No tasks to do");
   updateTaskCategories("inProgress", "inProgress", "No tasks in progress");
-  updateTaskCategories("awaitFeedback", "awaitFeedback", "No tasks await feedback");
+  updateTaskCategories(
+    "awaitFeedback",
+    "awaitFeedback",
+    "No tasks await feedback"
+  );
   updateTaskCategories("done", "done", "No tasks done");
 }
-
 
 function startDragging(id) {
   currentDraggedElement = id;
 }
 
-
 function allowDrop(ev) {
   ev.preventDefault();
 }
-
 
 async function moveTo(status) {
   let task = tasks.find((task) => task.id == currentDraggedElement);
@@ -134,7 +133,6 @@ async function moveTo(status) {
   }
 }
 
-
 function searchTasks(inputValue) {
   emptyDragAreaWhileSearching();
   currentSearchInput = inputValue.toLowerCase();
@@ -145,19 +143,19 @@ function searchTasks(inputValue) {
     if (titleElement || descriptionElement) {
       const title = titleElement.textContent.trim().toLowerCase();
       const description = descriptionElement.textContent.trim().toLowerCase();
-      const isVisible = title.includes(currentSearchInput) || description.includes(currentSearchInput);
+      const isVisible =
+        title.includes(currentSearchInput) ||
+        description.includes(currentSearchInput);
       taskCard.style.display = isVisible ? "block" : "none";
     }
   });
 }
-
 
 function applyCurrentSearchFilter() {
   if (currentSearchInput) {
     searchTasks(currentSearchInput);
   }
 }
-
 
 function emptyDragAreaWhileSearching() {
   let dragAreas = document.querySelectorAll(".noTaskPlaceholder");
@@ -169,27 +167,24 @@ function emptyDragAreaWhileSearching() {
   });
 }
 
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  const overlay = document.getElementById('overlay');
-  const addTaskOverlay = document.getElementById('addTaskOverlay');
+  const overlay = document.getElementById("overlay");
+  const addTaskOverlay = document.getElementById("addTaskOverlay");
   if (event.target === overlay || event.target === addTaskOverlay) {
-      closeModal();
+    closeModal();
   }
 };
 
-
 function closeModal() {
-  const overlay = document.getElementById('overlay');
-  const addTaskOverlay = document.getElementById('addTaskOverlay');
+  const overlay = document.getElementById("overlay");
+  const addTaskOverlay = document.getElementById("addTaskOverlay");
   if (overlay || addTaskOverlay) {
-      overlay.style.display = "none";
-      addTaskOverlay.style.display = "none";
+    overlay.style.display = "none";
+    addTaskOverlay.style.display = "none";
   }
   document.body.classList.remove("modalOpen");
 }
-
 
 function openOverlay(elementId) {
   let element = tasks.find((task) => task.id === elementId);
@@ -198,13 +193,11 @@ function openOverlay(elementId) {
   overlay.style.display = "block";
 }
 
-
 async function openAddTaskOverlay() {
   let addTaskOverlay = document.getElementById("addTaskOverlay");
   addTaskOverlay.innerHTML = await fetchAddTaskTemplate();
   addTaskOverlay.style.display = "block";
 }
-
 
 function checkScreenWidth(category) {
   const screenWidth = window.innerWidth;
@@ -212,85 +205,48 @@ function checkScreenWidth(category) {
   taskStatus = category;
   if (screenWidth < 992) {
     changeActive(activeTab);
-    return window.location.href = "../html/addtask.html";
+    return (window.location.href = "../html/addtask.html");
   } else {
     openAddTaskOverlay();
   }
 }
 
-
 async function updateSubtaskStatus(taskId, subtaskIndex) {
   let task = tasks.find((task) => task.id === taskId);
   if (task) {
-      let subtask = task.subtasks[subtaskIndex];
-      if (subtask) {
-          subtask.status = subtask.status === 'checked' ? 'unchecked' : 'checked';
-          let subtaskCheckbox = document.getElementById(`subtaskCheckbox${subtaskIndex}`);
-          if (subtaskCheckbox) {
-              subtaskCheckbox.src = subtask.status === 'checked' ? '../assets/icons/checkboxchecked.svg' : '../assets/icons/checkbox.svg';
-          }
-          updateSubtasksProgressBar(task.subtasks, taskId);
-          await updateTaskInFirebase(taskId, task);
+    let subtask = task.subtasks[subtaskIndex];
+    if (subtask) {
+      subtask.status = subtask.status === "checked" ? "unchecked" : "checked";
+      let subtaskCheckbox = document.getElementById(
+        `subtaskCheckbox${subtaskIndex}`
+      );
+      if (subtaskCheckbox) {
+        subtaskCheckbox.src =
+          subtask.status === "checked"
+            ? "../assets/icons/checkboxchecked.svg"
+            : "../assets/icons/checkbox.svg";
       }
+      updateSubtasksProgressBar(task.subtasks, taskId);
+      await updateTaskInFirebase(taskId, task);
+    }
   }
 }
 
-
 function updateSubtasksProgressBar(subtasks, taskId) {
-  let checkedAmt = subtasks.filter(subtask => subtask.status === 'checked').length;
+  let checkedAmt = subtasks.filter(
+    (subtask) => subtask.status === "checked"
+  ).length;
   let percent = Math.round((checkedAmt / subtasks.length) * 100);
-  document.getElementById(`subtasksProgressbarProgress${taskId}`).style.width = `${percent}%`;
-  document.getElementById(`subtasksProgressbarText${taskId}`).innerHTML = `${checkedAmt}/${subtasks.length} Subtasks`;
+  document.getElementById(
+    `subtasksProgressbarProgress${taskId}`
+  ).style.width = `${percent}%`;
+  document.getElementById(
+    `subtasksProgressbarText${taskId}`
+  ).innerHTML = `${checkedAmt}/${subtasks.length} Subtasks`;
 }
 
-
 function enableTaskEdit(id) {
-  let modalContainer = document.getElementById('modalContainer');
+  let modalContainer = document.getElementById("modalContainer");
   modalContainer.innerHTML = generateTaskEditHTML(id);
 }
 
-
-
-// in progress
-function generateTaskEditHTML(id) {
-  return /*html*/ `
-    <div class="modalToDoContent">
-
-      <div class="modalCategoryContainer">
-        <img class="modalCloseIcon" onclick="closeModal()" src="../assets/icons/clearIcon.svg" alt="">
-      </div>
-      <div class="modalScrollbarWrapper">
-        <div class="modalHeader"></div>
-        <div class="modalDescription" id="modalDescription"></div>
-        <div class="modalDateContainer">
-          <div class="modalDateText">Due date:</div>
-          <div></div>
-        </div>
-        <div class="modalPrioContainer">
-          <div class="modalPrioText">Priority:</div>
-          <div class="modalPrioIconContainer">
-            <div></div>
-            <img src="../assets/icons/priolowsmall.svg">
-          </div>
-        </div>
-        <div class="modalAssignedToContainer">
-          <div class="modalAssignedToText">Assigned To:</div>
-          <div class="modalAssignedToContainer"></div>
-        </div>
-        <div>
-          <div class="modalSubtasksText">Subtasks</div>
-          <div class="modalSubtasksContainer"></div>
-        </div>
-      </div>
-
-      <div class="editBottomContainer">
-      <div class="saveTaskEditBtn" onclick="">
-          <div>Ok</div>
-          <img src="../assets/icons/check.svg">
-      </div>
-    </div>
-      </div>
-    </div>
-  `;
-  
-}
