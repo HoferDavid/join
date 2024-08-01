c500f737e89243985ad88b1816a99be39e5369fb
David Hofer <chassis.poppets0x@icloud.com>
2024-08-01 14:03:41 +0200

    Add Task Bug Fix

--- script/addTask.js
+++ script/addTask.js
@@ -103,7 +103,7 @@ function showTaskAddedAnimationModal() {
 
 
 async function closeAddTaskModal() {
-  if (activeTab == 'Add Task') {
+  if (activeTab == 'add task') {
     showTaskAddedAnimation();
     tasks = [];
     await pushDataToArray();
